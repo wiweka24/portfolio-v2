14a875826d21e98f3f08164218d9acbc6c6298d2
wiweka24 <yogomu24@gmail.com>
2026-04-28 23:45:30 +0700

    feat: config route

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BottomNav from "./components/BottomNav";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio">
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/drawings" element={<Drawings />} />
